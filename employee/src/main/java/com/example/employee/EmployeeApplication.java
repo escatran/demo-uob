12d8327a85707a8d9d9dab05d112a0a977acd9c4
tungtran <tungdtran89@gmail.com>
2019-05-18 16:30:27 +0700

    add camel route

--- employee/src/main/java/com/example/employee/EmployeeApplication.java
+++ employee/src/main/java/com/example/employee/EmployeeApplication.java
@@ -1,9 +1,11 @@
 package com.example.employee;
 
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 
 @SpringBootApplication
@@ -15,15 +17,27 @@ public class EmployeeApplication {
 
 	class EmployeeCheckingRouteBuilder extends RouteBuilder {
 
+		public static final String TO_EMPLOYEE_DATA_SERVICE_CHECK_EXIST = "direct:checkExist";
+
 		@Override
 		public void configure() throws Exception {
 			restConfiguration()
 					.component("servlet")
 					.bindingMode(RestBindingMode.json);
 
-//			controller("/employee").produces(MediaType.APPLICATION_JSON.toString())
-//					.post("/")
-//						.to
+			rest("/employee")
+					.produces(MediaType.APPLICATION_JSON.toString())
+					.consumes(MediaType.APPLICATION_JSON.toString())
+					.post("/")
+					.route().to(TO_EMPLOYEE_DATA_SERVICE_CHECK_EXIST);
+
+			from(TO_EMPLOYEE_DATA_SERVICE_CHECK_EXIST)
+					.setHeader(Exchange.HTTP_METHOD, constant(HttpMethod.POST.toString()))
+					.setHeader(Exchange.CONTENT_TYPE, constant(MediaType.APPLICATION_JSON_VALUE))
+					.to("http://localhost:8090/employee/exist")
+					.process(exchange -> {
+						int statusCode = exchange.getOut().getHeader(Exchange.HTTP_RESPONSE_CODE, Integer.class);
+					});
 		}
 	}
 }
