38c6d71c7f865a92b27c94a36eb9c44d2bc4fd9f
tungtran <tungdtran89@gmail.com>
2019-05-18 17:02:42 +0700

    add camel route

--- employee/src/main/java/com/example/employee/EmployeeApplication.java
+++ employee/src/main/java/com/example/employee/EmployeeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class EmployeeApplication {
@@ -15,6 +16,7 @@ public class EmployeeApplication {
 		SpringApplication.run(EmployeeApplication.class, args);
 	}
 
+	@Component
 	class EmployeeCheckingRouteBuilder extends RouteBuilder {
 
 		public static final String TO_EMPLOYEE_DATA_SERVICE_CHECK_EXIST = "direct:checkExist";
@@ -34,7 +36,7 @@ public class EmployeeApplication {
 			from(TO_EMPLOYEE_DATA_SERVICE_CHECK_EXIST)
 					.setHeader(Exchange.HTTP_METHOD, constant(HttpMethod.POST.toString()))
 					.setHeader(Exchange.CONTENT_TYPE, constant(MediaType.APPLICATION_JSON_VALUE))
-					.to("http://localhost:8090/employee/exist")
+					.to("http://localhost:8090/employee-data/exist")
 					.process(exchange -> {
 						int statusCode = exchange.getOut().getHeader(Exchange.HTTP_RESPONSE_CODE, Integer.class);
 					});
