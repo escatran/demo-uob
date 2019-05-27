53d0ced346ef9b38186d6e658a5a9656a3ee8a84
Tung Tran <tungdtran89@outlook.com>
2019-05-27 11:35:22 +0700

    finish client code

--- uob-client/src/main/java/com/example/uobclient/controller/TestController.java
+++ uob-client/src/main/java/com/example/uobclient/controller/TestController.java
@@ -1,9 +1,12 @@
 package com.example.uobclient.controller;
 
+import com.example.employee.dto.CheckEmployeeResult;
+import com.example.employee.dto.EmployeeDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,8 +20,21 @@ public class TestController {
     @Autowired
     private RestTemplate restTemplate;
 
+    private ObjectMapper objectMapper = new ObjectMapper();
+
     @GetMapping
     public String home() throws RestClientException, URISyntaxException {
         return restTemplate.getForObject(new URI("https://localhost:8000/ping"), String.class);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<CheckEmployeeResult> createEmployee(@RequestBody EmployeeDto employeeDto) throws JsonProcessingException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        String payload = objectMapper.writeValueAsString(employeeDto);
+        HttpEntity<Object> request = new HttpEntity<Object>(payload, headers);
+
+        ResponseEntity<CheckEmployeeResult> rspn = restTemplate.exchange("https://localhost:8000/emp", HttpMethod.POST, request, CheckEmployeeResult.class);
+        return ResponseEntity.status(rspn.getStatusCode()).body(rspn.getBody());
     }
 }
